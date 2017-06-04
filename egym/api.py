722b74a15685c049fa7d2c341f3cedba0d22993a
bitstacker <bitstacker@never-afk.de>
2017-06-04 21:19:51 +0200

    stuff

--- egym/api.py
+++ egym/api.py
@@ -4,6 +4,7 @@ import requests
 
 class Api(object):
     """A python interface into the eGym API"""
+    userid = None
 
     def __init__(self,
             email=None,
@@ -17,9 +18,12 @@ class Api(object):
 
         self.email = email
         self.password = password
-        self.GetAccessToken()
+        if token is None:
+            self.GetUserLogin()
+        else:
+            self.token = token
 
-    def GetAccessToken(self):
+    def GetUserLogin(self):
         endpoint = 'auth/login'
         post_headers = {
             'User-Agent': 'Mozilla/5.0 (Linux; Android 7.0; Pixel C Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.98 Safari/537.36',
@@ -27,7 +31,18 @@ class Api(object):
         res = requests.post(url=self.base_url+endpoint,
                             json={'email': self.email, 'password': self.password},
                             headers=post_headers)
-        return res.json()
+        self.token = res.json()['accessToken']
+        self.userid = res.json()['userId']
 
     def GetUserActivities(self):
         return "Bla"
+
+    def GetUserSessions(self):
+        endpoint = 'user/sessions'
+        post_headers = {
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 7.0; Pixel C Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.98 Safari/537.36',
+            'Content-Type': 'application/json'}
+        res = requests.get(url=self.base_url+endpoint,
+                            json={'token': self.token},
+                            headers=post_headers)
+        return res.json()
