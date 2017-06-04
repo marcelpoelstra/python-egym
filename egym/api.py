0e9948480257f380b091bd950eacb667eb80f412
bitstacker <bitstacker@never-afk.de>
2017-06-04 22:26:25 +0200

    add profile and dashboard

--- egym/api.py
+++ egym/api.py
@@ -1,24 +1,6 @@
 #!/usr/bin/env python
 
 import requests
-import logging
-
-# These two lines enable debugging at httplib level (requests->urllib3->http.client)
-# You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
-# The only thing missing will be the response.body which is not logged.
-try:
-    import http.client as http_client
-except ImportError:
-    # Python 2
-    import httplib as http_client
-http_client.HTTPConnection.debuglevel = 1
-
-# You must initialize logging, otherwise you'll not see debug output.
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
-requests_log = logging.getLogger("requests.packages.urllib3")
-requests_log.setLevel(logging.DEBUG)
-requests_log.propagate = True
 
 class Api(object):
     """A python interface into the eGym API"""
@@ -54,7 +36,21 @@ class Api(object):
         endpoint = 'user/sessions'
         headers = self.buildHeaders()
         res = requests.get(url=self.base_url+endpoint,
-                            data={'end': '04.06.2017'},
+                            json={'start': '1494201600000', 'end': '1494201600000'},
+                            headers=headers)
+        return res.json()
+
+    def GetUserDashboard(self):
+        endpoint = 'user/dashboard'
+        headers = self.buildHeaders()
+        res = requests.get(url=self.base_url+endpoint,
+                            headers=headers)
+        return res.json()
+
+    def GetUserProfile(self):
+        endpoint = 'user/profile'
+        headers = self.buildHeaders()
+        res = requests.get(url=self.base_url+endpoint,
                             headers=headers)
         return res.json()
 
